37d7a6f81e5ca968a1a37b06bcce4d3913aca9cb
shitalbale <shitalbale@gmail.com>
2023-08-21 11:50:36 +0530

    LOG IN DONE

--- LoanApplication/src/app/app-routing.module.ts
+++ LoanApplication/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { AboutUsComponent } from './template/about-us/about-us.component';
 import { ContactUsComponent } from './template/contact-us/contact-us.component';
 import { LoginComponent } from './entry/login/login.component';
 import { AppDashBoardComponent } from './entry/app-dash-board/app-dash-board.component';
+import { EnquiryComponent } from './template/enquiry/enquiry.component';
 
 const routes: Routes = [
 {
   path:'', component:HomeComponent ,children:[
                                               {path:'about',component:AboutUsComponent},
                                               {path:'contact',component:ContactUsComponent},
-                                              {path:'login',component:LoginComponent}
+                                              {path:'login',component:LoginComponent},
+                                              {path:'enquiry',component:EnquiryComponent}
                                             ]
 },
 {
@@ -19,24 +21,32 @@ const routes: Routes = [
  
     {
       path:'oe' , loadChildren:()=>import('src/app/modules/oe/oe.module').then(e=>e.OeModule)
-    }
+    },
     
-  ]
-},
-{
-  path:'dash' , component:AppDashBoardComponent ,children:[
- 
     {
       path:'re' , loadChildren:()=>import('src/app/modules/re/re.module').then(e=>e.ReModule)
-    }
-  ]
-},
-{
-  path:'dash' , component:AppDashBoardComponent ,children:[
- 
+    },
     {
       path:'admin' , loadChildren:()=>import('src/app/modules/admin/admin.module').then(e=>e.AdminModule)
     }
+    
+    
+  // ]
+// },
+// {
+//   path:'dash' , component:AppDashBoardComponent ,children:[
+ 
+//     {
+//       path:'re' , loadChildren:()=>import('src/app/modules/re/re.module').then(e=>e.ReModule)
+//     }
+//   ]
+// },
+// {
+//   path:'dash' , component:AppDashBoardComponent ,children:[
+ 
+//     {
+//       path:'admin' , loadChildren:()=>import('src/app/modules/admin/admin.module').then(e=>e.AdminModule)
+//     }
   ]
 }
 
--- LoanApplication/src/app/template/enquiry/enquiry.component.ts
+++ LoanApplication/src/app/template/enquiry/enquiry.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoanService } from 'src/app/service/loan.service';
 
 @Component({
   selector: 'app-enquiry',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./enquiry.component.css']
 })
 export class EnquiryComponent {
+  constructor(private fb:FormBuilder,private router:Router, private loanService:LoanService){}
+  enquiryForm:FormGroup;
+  
+  ngOnInit(): void {
+    this.enquiryForm=this.fb.group({
+    
+    })
+  }
 
+  public addEnquiryInfo()
+  {this.loanService.addEnquiry(this.fb).subscribe(
+    (enquiry: any) => {
+      if (enquiry.eId > 0) {
+        alert("user created...")
+        this.enquiryForm.reset();
+      }
+    }
+  );
 }
+
+  }
+
