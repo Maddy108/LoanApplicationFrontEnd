a4ea94891be943dc59a3c2db5c763c8d621acf9c
shitalbale <shitalbale@gmail.com>
2023-08-24 13:19:12 +0530

    cm component

---
+++ LoanApplication/src/app/modules/cm/check-appl/check-appl.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check-appl',
+  templateUrl: './check-appl.component.html',
+  styleUrls: ['./check-appl.component.css']
+})
+export class CheckApplComponent {
+
+}
--- LoanApplication/src/app/modules/cm/cm.module.ts
+++ LoanApplication/src/app/modules/cm/cm.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CmRoutingModule } from './cm-routing.module';
+import { CheckApplComponent } from './check-appl/check-appl.component';
+import { SanctionLetterComponent } from './sanction-letter/sanction-letter.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CheckApplComponent,
+    SanctionLetterComponent 
+  
+  ],
   imports: [
     CommonModule,
     CmRoutingModule
